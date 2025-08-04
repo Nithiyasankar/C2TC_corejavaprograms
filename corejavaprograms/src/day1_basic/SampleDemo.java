ea28f48379dbd3780ade3eb9837b66b7321bee35
DELL <DELL@DESKTOP-O0SDLDA>
2025-08-04 20:17:53 +0530

    Day 1

--- corejavaprograms/src/day1_basic/SampleDemo.java
+++ corejavaprograms/src/day1_basic/SampleDemo.java
@@ -1,11 +1,18 @@
 package day1_basic;
 
+import java.util.*;
+
+
 public class SampleDemo {
-
+	int sid;
+	String sname;
+	float avg;
 	public static void main(String[] args) {
-		System.out.println("");
-		// TODO Auto-generated method stub
-
+		int n;
+		SampleDemo s1=new SampleDemo();
+		System.out.println("Student Name: "+s1.sname);
+		System.out.println("Student Id: "+s1.sid);
+		System.out.println("Student Avg: "+s1.avg);
 	}
 
 }
