7dd1498209c9aca036a48b3856785d7f7b5cf006
DELL <DELL@DESKTOP-O0SDLDA>
2025-08-19 14:22:05 +0530

    I have done the single inheritence

---
+++ corejavaprograms/src/single_inheritence_example/SingleDemo.java
@@ -0,0 +1,11 @@
+package single_inheritence_example;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+	
+	Trainer t=new Trainer(122,"Nithiya","puducherry",78,"lead","Bangalore/Puducherry");
+	System.out.println(t);
+	}
+
+}
