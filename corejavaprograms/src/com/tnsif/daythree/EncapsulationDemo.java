48b0de9d984d99eb9da38c46a476131ae915c2b8
DELL <DELL@DESKTOP-O0SDLDA>
2025-08-19 15:18:18 +0530

    I have done TNS-IF day3 programs

---
+++ corejavaprograms/src/com/tnsif/daythree/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.daythree;
+
+import day3.OopsConceptDemo;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+
+            //object creation 
+            OopsConceptDemo obj = new OopsConceptDemo();
+            obj.setSerialNum(101);
+            obj.setName("Nithiya");
+            obj.setAge(21);
+            System.out.println(obj);
+
+    }
+}
