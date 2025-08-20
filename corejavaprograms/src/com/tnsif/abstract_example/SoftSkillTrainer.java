7e3ea436939c0fcad05c1782d2d5221b3c8fc98d
DELL <DELL@DESKTOP-O0SDLDA>
2025-08-20 14:06:40 +0530

    I have completed abstration program

---
+++ corejavaprograms/src/com/tnsif/abstract_example/SoftSkillTrainer.java
@@ -0,0 +1,13 @@
+package com.tnsif.abstract_example;
+
+public class SoftSkillTrainer extends Trainer {
+	
+	public void session() {
+		System.out.println("Resume building is going on");
+	}
+	public void checks()
+	{
+		System.out.println("Cover letter assignment");
+	}
+
+}
